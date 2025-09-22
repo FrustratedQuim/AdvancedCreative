6d7ecc172e2637c049951d511a627e6c1cc8aabf
ruViolence <78062896+ruViolence@users.noreply.github.com>
2025-09-22 20:24:16 +0800

    fix: do not touch PacketEvents startup logic
    As it's not shaded into the plugin

--- src/main/kotlin/com/ratger/acreative/AdvancedCreative.kt
+++ src/main/kotlin/com/ratger/acreative/AdvancedCreative.kt
@@ -1,7 +1,6 @@
 package com.ratger.acreative
 
 import com.github.retrooper.packetevents.PacketEvents
-import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder
 import me.tofaa.entitylib.EntityLib
 import org.bukkit.plugin.java.JavaPlugin
 import com.ratger.acreative.core.FunctionHooker
@@ -12,14 +11,7 @@ class AdvancedCreative : JavaPlugin() {
 
     lateinit var functionHooker: FunctionHooker
 
-    override fun onLoad() {
-        PacketEvents.setAPI(SpigotPacketEventsBuilder.build(this))
-        PacketEvents.getAPI().load()
-    }
-
     override fun onEnable() {
-        PacketEvents.getAPI().init()
-
         val platform = SpigotEntityLibPlatform(this)
         val settings = APIConfig(PacketEvents.getAPI())
             .tickTickables()
@@ -32,6 +24,5 @@ class AdvancedCreative : JavaPlugin() {
 
     override fun onDisable() {
         functionHooker.shutdown()
-        PacketEvents.getAPI().terminate()
     }
 }
