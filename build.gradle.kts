08714d14098f78203ad55df81332e9eed555241c
FrustratedQuim <pokionch@gmail.com>
2026-03-15 15:47:23 +0700

    work

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "2.2.10"
     id("com.gradleup.shadow") version "9.0.2"
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.18"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.17"
 }
 
 group = "com.ratger.acreative"
@@ -9,6 +9,7 @@ version = "1.1.1"
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://jitpack.io")
     maven("https://repo.codemc.io/repository/maven-releases/")
     maven("https://repo.codemc.io/repository/maven-snapshots/")
@@ -19,8 +20,9 @@ dependencies {
     paperweight.paperDevBundle("1.21.1-R0.1-SNAPSHOT")
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.2.10")
-    implementation("com.github.Tofaa2.EntityLib:spigot:master-6fba8ea5fdc7880d1c62c3428f562fea2745b58b") // 2.4.11
+    implementation("com.github.Tofaa2.EntityLib:spigot:2.4.11")
     compileOnly("com.github.retrooper:packetevents-spigot:2.9.4")
+    compileOnly("net.kyori:adventure-text-minimessage:4.26.1")
 }
 
 java {
