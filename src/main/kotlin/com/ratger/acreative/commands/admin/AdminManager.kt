6e423b8f86144b0587de41ca3f0d38aa657229cd
FrustratedQuim <pokionch@gmail.com>
2026-05-01 17:49:07 +0700

    work

--- src/main/kotlin/com/ratger/acreative/commands/admin/AdminManager.kt
+++ src/main/kotlin/com/ratger/acreative/commands/admin/AdminManager.kt
@@ -42,10 +42,11 @@ class AdminManager(
 
     fun showToggleStatus(player: Player) {
         player.sendMessage(mini.deserialize("<#FFD700><st>                          </st><<#FFE68A><b> Toggle Status </b><#FFD700>><st>                         </st>"))
+        player.sendMessage(mini.deserialize("<#FFE68A><b>Актуальные:</b>"))
 
         hooker.systemToggleService.statuses().forEach { status ->
-            val stateText = if (status.enabled) "<#00FF40>Вкл" else "<#FF1500>Выкл"
-            val line = "<#C7A300> ● <#FFE68A>${status.system.displayName}<#EDC800>- $stateText"
+            val stateText = if (status.enabled) "<#00FF40>Включено" else "<#FF1500>Отключено"
+            val line = "<#C7A300> ● <#FFE68A>${status.system.displayName} <#EDC800>- $stateText"
             player.sendMessage(mini.deserialize(line))
         }
 
